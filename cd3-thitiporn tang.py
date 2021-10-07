3aed1ba0f1c727177e36d972572a550dc6d91ff9
Thitiporn3131 <92080688+Thitiporn3131@users.noreply.github.com>
2021-10-07 15:46:15 +0700

    Add files via upload

---
+++ cd3-thitiporn tang.py
@@ -0,0 +1,47 @@
+#foundation eng (founeng)
+#general bussiness (genbussi)
+#intro to computer system (introcom)
+#computer programming (compro)
+
+name = input("Enter your name: ")
+#john
+#sue
+#jennie
+if name == "john" :
+    print("Score of " + name)
+    print("---Your score---")
+    founeng = float(30.5)
+    genbussi = float(67)
+    introcom = float(70)
+    compro = float(50.4)
+    print("Foundation English : " , founeng)
+    print("General Bussiness : " , genbussi)
+    print("Introduction to Computer System : " , introcom)
+    print("Computer Programming : " , compro)
+
+elif name == "sue":
+    print("Score of " + name)
+    print("---Your score---")
+    founeng = float(70)
+    genbussi = float(64.3)
+    introcom = float(73)
+    compro = float(58)
+    print("Foundation English : " , founeng)
+    print("General Bussiness : " , genbussi)
+    print("Introduction to Computer System : " , introcom)
+    print("Computer Programming : " , compro)
+
+else:
+    print("Score of " + name)
+    print("---Your score---")
+    founeng = float(89)
+    genbussi = float(67)
+    introcom = float(79.1)
+    compro = float(50.4)
+    print("Foundation English : " , founeng)
+    print("General Bussiness : " , genbussi)
+    print("Introduction to Computer System : " , introcom)
+    print("Computer Programming : " , compro)
+
+
+
